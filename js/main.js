aa788402835f6b596d7dd259de5375ea0d4f5767
Darin H. Hajou <it_related@hotmail.com>
2022-12-07 10:28:14 +0100

    Created slideshow in index and linked to main

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import Mainpage from './modules/mainpage.js';
 import Sidebar from './modules/sidebar.js';
 import Toolbar from './modules/toolbar.js';
+import Slideshow from './modules/slideshow.js';
 
 Mainpage();
 Sidebar();
 Toolbar();
+Slideshow();
