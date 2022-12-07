eefbc68e639e041cfed9b26376d9758e67d81adb
Darin H. Hajou <it_related@hotmail.com>
2022-12-07 10:08:23 +0100

    Added style attributes toolbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import Mainpage from './modules/mainpage.js';
 import Sidebar from './modules/sidebar.js';
+import Toolbar from './modules/toolbar.js';
 
 Mainpage();
 Sidebar();
+Toolbar();
---
+++ js/modules/toolbar.js
@@ -0,0 +1,6 @@
+export default function() {
+	let toolbar = document.querySelector('.toolbar');
+	toolbar.onclick = function() {
+		toolbar.classList.toggle('active');
+	}
+}
