ccd85c563785290c8e9a3ca9e61592566f4b3b1b
Darin H. Hajou <it_related@hotmail.com>
2022-12-09 00:16:06 +0100

    sd

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import Mainpage from './modules/mainpage.js';
-import Sidebar from './modules/sidebar.js';
-import Toolbar from './modules/toolbar.js';
-import Slideshow from './modules/slideshow.js';
-import Cards from './modules/cards.js';
+import Mainpage from '/modules/mainpage.js';
+import Sidebar from '/modules/sidebar.js';
+import Toolbar from '/modules/toolbar.js';
+import Slideshow from '/modules/slideshow.js';
+import Cards from '/modules/cards.js';
 
 Mainpage();
 Sidebar();
