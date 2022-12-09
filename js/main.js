e7b545141eae4d17499348a034233228f7b3cddb
Darin H. Hajou <it_related@hotmail.com>
2022-12-09 01:20:03 +0100

    sdsdd

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-import Mainpage from '../js/modules/mainpage.js';
-import Sidebar from '../js/modules/sidebar.js';
-import Toolbar from '../js/modules/toolbar.js';
-import Slideshow from '../js/modules/slideshow.js';
-import Cards from '../js/modules/cards.js';
+import Mainpage from './modules/mainpage.js';
+
 
 Mainpage();
-Sidebar();
-Toolbar();
-Slideshow();
-Cards();
